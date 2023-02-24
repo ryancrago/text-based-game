7b6d39bbc4410b3d07344f4f8d25138139614bf8
ryancrago <ryancrago@icloud.com>
2023-02-24 11:17:30 -0700

    added some new inputs and prints

--- game.py
+++ game.py
@@ -1,4 +1,19 @@
+import random
 def main():
-	print(f"")
-	name = input("What will your detective's name be?\n")
-	
+	inventory = []
+	print(f"You find yourself stuck in a cave that collapsed. Search the cave to find any resources that can help you escape. You've only ever heard rumors of monsters dwelling in these caves. So be careful while you're exploring who knows what's down there.")
+	name = input("What will your adventurer's name be?\n")
+	print(f"Welcome brave adventurer {name}.")
+	print(f"You look at your surroundings. Convienently, you find a torch on the wall.")
+	torch = input(f"Would you like to take the torch with you? y/n\n")
+	if torch == "y":
+		inventory.append(torch)
+		print("*Torch was added to your inventory*")
+	elif torch == "n":
+		print("You decide to leave the torch behind.")
+	else:
+		print(f"That is not a wise decision {name}")
+	print(f"Now that you can finally see the whole room, you see that there is a hole that you can fit through.")
+	hole = input(f"Would you like to pass through the hole? y/n\n")
+	if hole == "y":
+		print
