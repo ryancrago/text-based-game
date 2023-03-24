f8520f4fc29888d96c48b330a7736074e19ff5e8
ryancrago <ryancrago@icloud.com>
2023-03-24 11:12:35 -0600

    updated code so that it does more internal things

--- game.py
+++ game.py
@@ -1,19 +1,34 @@
-import random
-def main():
-	inventory = []
-	print(f"You find yourself stuck in a cave that collapsed. Search the cave to find any resources that can help you escape. You've only ever heard rumors of monsters dwelling in these caves. So be careful while you're exploring who knows what's down there.")
-	name = input("What will your adventurer's name be?\n")
-	print(f"Welcome brave adventurer {name}.")
-	print(f"You look at your surroundings. Convienently, you find a torch on the wall.")
-	torch = input(f"Would you like to take the torch with you? y/n\n")
-	if torch == "y":
-		inventory.append(torch)
-		print("*Torch was added to your inventory*")
-	elif torch == "n":
-		print("You decide to leave the torch behind.")
-	else:
-		print(f"That is not a wise decision {name}")
-	print(f"Now that you can finally see the whole room, you see that there is a hole that you can fit through.")
-	hole = input(f"Would you like to pass through the hole? y/n\n")
-	if hole == "y":
-		print
+actions = ("quit"
+           "save"
+           "load"
+           "take"
+           "use"
+           "move")
+class Player():
+    def __init__(self):
+        self.position = (0, 0, 0)
+        self.inventory = []
+        self.name = input("What will your adventurer's name be?\n")
+        print(f"Welcome brave adventurer, {self.name}")
+    def move(self, move):
+        self.position = tuple(array(self.position) + MOVEMENT.get(move, array([0,0,0])))
+
+player = Player()	    
+def main(player):
+    choice = None
+    while choice != "quit":
+        room = rooms.get(player.position, "Invalid room setting - something broke")
+        print(room.description())
+        choice = valid_input()
+        if choice == "quit":
+            print("Thanks for playing!")
+        elif choice == "save":
+            save()
+        elif choice == "load":
+            load()
+        elif choice == "take":
+            room.get_item(player)
+        elif choice == "use":
+            room.use_item(player)
+        elif choice == "move":
+            room.move(player)
